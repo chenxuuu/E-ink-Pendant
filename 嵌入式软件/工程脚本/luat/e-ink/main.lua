0f49492c5fdbd2f08ee4ab14d3176ea24dfed618
chenxuuu <lolicon@papapoi.com>
2018-07-21 12:11:19 +0800

    嵌入式脚本功能完成

--- 嵌入式软件/工程脚本/luat/e-ink/main.lua
+++ 嵌入式软件/工程脚本/luat/e-ink/main.lua
@@ -26,8 +26,8 @@ wdt.setup(pio.P0_30, pio.P0_31)
 --加载网络指示灯功能模块
 --根据自己的项目需求和硬件配置决定：1、是否加载此功能模块；2、配置指示灯引脚
 --合宙官方出售的Air800和Air801开发板上的指示灯引脚为pio.P0_28，其他开发板上的指示灯引脚为pio.P1_1
---require "netLed"
---netLed.setup(true,pio.P1_1)
+require "netLed"
+netLed.setup(true,pio.P1_1)
 --网络指示灯功能模块中，默认配置了各种工作状态下指示灯的闪烁规律，参考netLed.lua中ledBlinkTime配置的默认值
 --如果默认值满足不了需求，此处调用netLed.updateBlinkTime去配置闪烁时长
 
@@ -38,11 +38,12 @@ errDump.request("udp://ota.airm2m.com:9072")
 
 --加载远程升级功能模块【强烈建议打开此功能】
 --如下3行代码，只是简单的演示如何使用update功能，详情参考update的api以及demo/update
---PRODUCT_KEY = "v32xEAKsGTIEQxtqgwCldp5aPlcnPs3K"
---require "update"
---update.request()
+PRODUCT_KEY = "a79KxY1I3CJ5m6KymLWLfYZGn0lcqNZz"
+require "update"
+update.request()
 
 require "epd1in54"
+require "picdata"
 
 --启动系统框架
 sys.init(0, 0)
